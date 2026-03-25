12110bef4fcc0fa8498eff11f6e14456497bfe97
Camilo-lopez239 <murillocamilo393@gmail.com>
2026-03-25 12:03:04 -0500

    Fix: Forzando variable de entorno y ruta estátia

--- src/Index.js
+++ src/Index.js
@@ -12,7 +12,7 @@ app.set('port', process.env.PORT || 3000);
 const server = http.createServer(app);
 const io = socketio(server);
 const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test';
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI)
     .then(db => console.log('Base de datos conectada'))
     .catch(err => console.log('Error en DB', err))
 require('./Sockets.js')(io);
